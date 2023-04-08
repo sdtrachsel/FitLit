392425fb672f292e623cfc6cf2d102ccea32daea
Sharie Trachsel <strachsel22@turing.edu>
2023-04-08 15:46:25 -0500

    Set up API

--- src/Activity.js
+++ src/Activity.js
@@ -5,20 +5,20 @@ class Activity {
         this.activityLogs = activityFile.activityData.filter(log => userInfo.id  === log.userID);
     }
 
-    milesPerDay(day){
-        const selectedDay = this.activityLogs.find(log => log.date === day);
-        const miles = (selectedDay.numSteps * this.userInfo.strideLength) / 5280;
+    // milesPerDay(day){
+    //     const selectedDay = this.activityLogs.find(log => log.date === day);
+    //     const miles = (selectedDay.numSteps * this.userInfo.strideLength) / 5280;
     
-        return Math.round(miles * 10) / 10;
-    }
+    //     return Math.round(miles * 10) / 10;
+    // }
 
-    activeMinutesByDay(date){
-        const dayDetail = this.activityLogs.find((log) => {
-            return log.date === date;
-        })
+    // activeMinutesByDay(date){
+    //     const dayDetail = this.activityLogs.find((log) => {
+    //         return log.date === date;
+    //     })
 
-        return dayDetail.minutesActive;
-    }
+    //     return dayDetail.minutesActive;
+    // }
 
     stepGoalReached(date){
         const dayDetail = this.activityLogs.find((log) => {
