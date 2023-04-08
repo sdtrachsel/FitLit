7558be67f05321c0837a31125ea672c43a95c9cb
Sharie Trachsel <strachsel22@turing.edu>
2023-04-08 16:18:48 -0500

    Add Activity code, finish Actvity test

--- src/Activity.js
+++ src/Activity.js
@@ -5,26 +5,25 @@ class Activity {
         this.activityLogs = activityFile.activityData.filter(log => userInfo.id  === log.userID);
     }
     
-    findMostRecentDay(){
-        return this.activityLogs[0].date
+    findMostRecentDay() {
+        return this.activityLogs[this.activityLogs.length - 1].date;
     }
 
+    calculateMilesPerDay(day){
+        const selectedDay = this.activityLogs.find(log => log.date === day);
+        const miles = (selectedDay.numSteps * this.userInfo.strideLength) / 5280;  
+        return Math.round(miles * 10) / 10;
+    }
 
-    // milesPerDay(day){
-    //     const selectedDay = this.activityLogs.find(log => log.date === day);
-    //     const miles = (selectedDay.numSteps * this.userInfo.strideLength) / 5280;  
-    //     return Math.round(miles * 10) / 10;
-    // }
+    findActiveMinutesByDay(date){
+        const dayDetail = this.activityLogs.find((log) => {
+            return log.date === date;
+        })
 
-    // activeMinutesByDay(date){
-    //     const dayDetail = this.activityLogs.find((log) => {
-    //         return log.date === date;
-    //     })
+        return dayDetail.minutesActive;
+    }
 
-    //     return dayDetail.minutesActive;
-    // }
-
-    stepGoalReached(date){
+    findStepGoalReachedDay(date){
         const dayDetail = this.activityLogs.find((log) => {
             return log.date === date
         });
@@ -33,6 +32,24 @@ class Activity {
         return stepGoal <= dayDetail.numSteps;
     }
 
-  }
-  
+    findStepsLastSevenDays() {
+        const sevenDayDetail = this.activityLogs.slice(-7).map(log => log.numSteps);
+
+        return sevenDayDetail
+    }
+
+    findStepGoalLastSevenDays() {
+        const sevenDayDetail = this.findStepsLastSevenDays()       
+
+        const sevenDayGoalDetail = sevenDayDetail.map((daySteps) => {
+            if (this.userInfo.dailyStepGoal <= daySteps) {
+                return true
+            } else {
+                return false
+            }
+        })
+        return sevenDayGoalDetail
+    };
+}
+ 
   export default Activity;
--- test/Activity-test.js
+++ test/Activity-test.js
@@ -11,16 +11,19 @@ describe('Activity', () => {
     beforeEach('data creation', () => {
         activityList = {
             activityData: [
-                { "userID": 1, "date": "2023/03/24", "numSteps": 7362, "minutesActive": 250, "flightsOfStairs": 26 },
-                { "userID": 2, "date": "2023/03/24", "numSteps": 5205, "minutesActive": 200, "flightsOfStairs": 16 },
+                { "userID": 1, "date": "2023/03/15", "numSteps": 6003, "minutesActive": 275, "flightsOfStairs": 23 },
+                { "userID": 1, "date": "2023/03/16", "numSteps": 4325, "minutesActive": 155, "flightsOfStairs": 10 },
+                { "userID": 1, "date": "2023/03/17", "numSteps": 7601, "minutesActive": 250, "flightsOfStairs": 26 },
+                { "userID": 1, "date": "2023/03/18", "numSteps": 3505, "minutesActive": 265, "flightsOfStairs": 22 },
+                { "userID": 3, "date": "2023/03/18", "numSteps": 5327, "minutesActive": 185, "flightsOfStairs": 14 },
+                { "userID": 1, "date": "2023/03/19", "numSteps": 3493, "minutesActive": 201, "flightsOfStairs": 15 },
+                { "userID": 1, "date": "2023/03/20", "numSteps": 9675, "minutesActive": 501, "flightsOfStairs": 22 },
+                { "userID": 1, "date": "2023/03/21", "numSteps": 4575, "minutesActive": 211, "flightsOfStairs": 17 },
+                { "userID": 1, "date": "2023/03/22", "numSteps": 5505, "minutesActive": 361, "flightsOfStairs": 15 },
                 { "userID": 1, "date": "2023/03/23", "numSteps": 6502, "minutesActive": 275, "flightsOfStairs": 23 },
                 { "userID": 2, "date": "2023/03/23", "numSteps": 4867, "minutesActive": 155, "flightsOfStairs": 10 },
-                { "userID": 1, "date": "2023/03/22", "numSteps": 5505, "minutesActive": 361, "flightsOfStairs": 15 },
-                { "userID": 1, "date": "2023/03/21", "numSteps": 4575, "minutesActive": 211, "flightsOfStairs": 17 },
-                { "userID": 1, "date": "2023/03/20", "numSteps": 9675, "minutesActive": 501, "flightsOfStairs": 22 },
-                { "userID": 1, "date": "2023/03/19", "numSteps": 3493, "minutesActive": 201, "flightsOfStairs": 15 },
-                { "userID": 3, "date": "2023/03/18", "numSteps": 5327, "minutesActive": 185, "flightsOfStairs": 14 },
-                { "userID": 1, "date": "2023/03/18", "numSteps": 3505, "minutesActive": 265, "flightsOfStairs": 22 }
+                { "userID": 1, "date": "2023/03/24", "numSteps": 7362, "minutesActive": 250, "flightsOfStairs": 26 },
+                { "userID": 2, "date": "2023/03/24", "numSteps": 5205, "minutesActive": 200, "flightsOfStairs": 16 }
             ]
         };
 
@@ -56,13 +59,16 @@ describe('Activity', () => {
 
     it('should store a users activity logs', function () {
         expect(testUser.activityLogs).to.deep.equal([
-            { "userID": 1, "date": "2023/03/24", "numSteps": 7362, "minutesActive": 250, "flightsOfStairs": 26 },
-            { "userID": 1, "date": "2023/03/23", "numSteps": 6502, "minutesActive": 275, "flightsOfStairs": 23 },
-            { "userID": 1, "date": "2023/03/22", "numSteps": 5505, "minutesActive": 361, "flightsOfStairs": 15 },
-            { "userID": 1, "date": "2023/03/21", "numSteps": 4575, "minutesActive": 211, "flightsOfStairs": 17 },
-            { "userID": 1, "date": "2023/03/20", "numSteps": 9675, "minutesActive": 501, "flightsOfStairs": 22 },
+            { "userID": 1, "date": "2023/03/15", "numSteps": 6003, "minutesActive": 275, "flightsOfStairs": 23 },
+            { "userID": 1, "date": "2023/03/16", "numSteps": 4325, "minutesActive": 155, "flightsOfStairs": 10 },
+            { "userID": 1, "date": "2023/03/17", "numSteps": 7601, "minutesActive": 250, "flightsOfStairs": 26 },
+            { "userID": 1, "date": "2023/03/18", "numSteps": 3505, "minutesActive": 265, "flightsOfStairs": 22 },
             { "userID": 1, "date": "2023/03/19", "numSteps": 3493, "minutesActive": 201, "flightsOfStairs": 15 },
-            { "userID": 1, "date": "2023/03/18", "numSteps": 3505, "minutesActive": 265, "flightsOfStairs": 22 }
+            { "userID": 1, "date": "2023/03/20", "numSteps": 9675, "minutesActive": 501, "flightsOfStairs": 22 },
+            { "userID": 1, "date": "2023/03/21", "numSteps": 4575, "minutesActive": 211, "flightsOfStairs": 17 },
+            { "userID": 1, "date": "2023/03/22", "numSteps": 5505, "minutesActive": 361, "flightsOfStairs": 15 },
+            { "userID": 1, "date": "2023/03/23", "numSteps": 6502, "minutesActive": 275, "flightsOfStairs": 23 },
+            { "userID": 1, "date": "2023/03/24", "numSteps": 7362, "minutesActive": 250, "flightsOfStairs": 26 }
         ]);
     });
 
@@ -71,16 +77,21 @@ describe('Activity', () => {
     })
 
     it('should find the miles per day when given a date', function () {
-        expect(testUser.milesPerDay('2023/03/22')).to.be.equal(4.2);
-        expect(testUser.milesPerDay('2023/03/18')).to.be.equal(2.7);
+        expect(testUser.calculateMilesPerDay('2023/03/22')).to.be.equal(4.2);
+        expect(testUser.calculateMilesPerDay('2023/03/18')).to.be.equal(2.7);
     })
 
-    it('should find the active minuts by day', function () {
-        expect(testUser.activeMinutesByDay('2023/03/21')).to.be.equal(211);
+    it('should find the active minutes by day', function () {
+        expect(testUser.findActiveMinutesByDay('2023/03/21')).to.be.equal(211);
+        expect(testUser.findActiveMinutesByDay('2023/03/17')).to.be.equal(250);
     });
 
     it('should find if they reached the step goal for a specific day', function () {
-        expect(testUser.stepGoalReached('2023/03/18')).to.be.equal(false);
-        expect(testUser.stepGoalReached('2023/03/24')).to.be.equal(true);
+        expect(testUser.findStepGoalReachedDay('2023/03/18')).to.be.equal(false);
+        expect(testUser.findStepGoalReachedDay('2023/03/24')).to.be.equal(true);
+    });
+
+    it('should find if they reached the step goal for the last seven days', function () {
+        expect(testUser.findStepGoalLastSevenDays()).to.deep.equal([false, false, true, false, false, false, true]);
     });
 });
