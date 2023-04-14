f0dba72ec8241d8f023092fd62927c37633cf023
Sharie Trachsel <strachsel22@turing.edu>
2023-04-14 14:13:43 -0500

    Add video playback after motivation form submit

---
+++ src/motivation-videos.js
@@ -0,0 +1,33 @@
+const motivationVideos = {
+    stillInBed: {
+        heading: 'Wake-up, ',
+        text: 'The world isn\'t going to conquer itself!',
+        song: 'Jump by Van Halen',
+        video: '<iframe src="https://www.youtube.com/embed/SwYN7mTi6HM?start=25&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    },
+    cantEven: {
+        heading: 'Hustle up, ',
+        text: 'The worms won\'t catch themselves!',
+        song: 'Don\'t stop Believen by Journey',
+        video: '<iframe src="https://www.youtube.com/embed/VcjzHMhBtf0?start=15&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    },
+    meh: {
+        heading: 'Watchout, ',
+        text: 'You are just warming up!',
+        song: 'Pump up the Jam by Technotronic',
+        video: '<iframe src="https://www.youtube.com/embed/9EcjWd-O4jI?start=16&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    },
+    given100: {
+        heading: 'Buckle-up, ',
+        text: 'You are flying!',
+        song: 'Push It by Salt N Peppa',
+        video: '<iframe width="560" height="315" src="https://www.youtube.com/embed/vCadcBR95oU?start=1&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    },
+    lfg: {
+        heading: "You did it, ",
+        text: 'BEAST MODE ACTIVATED!',
+        song: 'Welcome to the Jungle by Guns N Roses',
+        video: '<iframe src="https://www.youtube.com/embed/o1tj2zJ2Wvg?start=35&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>'
+    }
+}
+export default motivationVideos;
